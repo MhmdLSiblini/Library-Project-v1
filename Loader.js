fbb0dddc40b7e9e091d9ff107f2cc65f373e0692
Mohammad Siblini <mohammadsiblini246@gmail.com>
2022-06-07 03:05:23 +0300

    3rd commit

--- Loader.js
+++ Loader.js
@@ -2,10 +2,13 @@ let loader = document.querySelector('.loader');
 
 window.addEventListener('load', function() {
     loader.style.display='none';
-    document.querySelector('.about-me__picture--mask').classList.add('animate__profile');
-    document.querySelector('.about-me__info-title').classList.add('fade_up-title');
-    document.querySelector('.about-me__info--para').classList.add('fade_up--para');
-    document.querySelector('.about-me_links').classList.add('fade_up_links');
-    document.querySelector('.about-me__img').classList.add('fade_in__img');
     document.querySelector('#remove').classList.remove('loading');
 });
+
+
+//     For appearing animations
+//     document.querySelector('.about-me__picture--mask').classList.add('animate__profile');
+//     document.querySelector('.about-me__info-title').classList.add('fade_up-title');
+//     document.querySelector('.about-me__info--para').classList.add('fade_up--para');
+//     document.querySelector('.about-me_links').classList.add('fade_up_links');
+//     document.querySelector('.about-me__img').classList.add('fade_in__img');
